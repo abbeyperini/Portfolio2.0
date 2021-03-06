f3f427955e1d0dc34a82c67537762491b65933be
abbeyperini <abbeyperini@gmail.com>
2021-03-06 09:12:54 -0500

    themes.js refactor

--- portfolio/src/utils/themes.js
+++ portfolio/src/utils/themes.js
@@ -4,10 +4,12 @@ function setTheme(themeName) {
 }
 
 function keepTheme() {
-  if (localStorage.getItem('theme') && localStorage.getItem('theme') === 'theme-dark') {
-    setTheme('theme-dark');
-  } else if (localStorage.getItem('theme') && localStorage.getItem('theme') === 'theme-light') {
-    setTheme('theme-light')
+  if (localStorage.getItem('theme')) {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-dark');
+    } else if (localStorage.getItem('theme') === 'theme-light') {
+      setTheme('theme-light')
+    }
   } else {
     setTheme('theme-dark')
   }
