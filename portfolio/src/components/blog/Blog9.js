b73935a75c6451edd7cc263ea191b89c8b0b6b28
abbeyperini <abbeyperini@gmail.com>
2021-03-06 09:18:37 -0500

    update blog9 with theme.js refactor

--- portfolio/src/components/blog/Blog9.js
+++ portfolio/src/components/blog/Blog9.js
@@ -72,13 +72,15 @@ nav {
 }
 
 function keepTheme() {
-  if (localStorage.getItem('theme') && localStorage.getItem('theme') === 'theme-dark') {
-    setTheme('theme-dark');
-  } else if (localStorage.getItem('theme') && localStorage.getItem('theme') === 'theme-light') {
-    setTheme('theme-light')
-  } else {
-    setTheme('theme-dark')
-  }
+    if (localStorage.getItem('theme')) {
+      if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+      } else if (localStorage.getItem('theme') === 'theme-light') {
+        setTheme('theme-light')
+      }
+    } else {
+      setTheme('theme-dark')
+    }
 }
 
 module.exports = {
