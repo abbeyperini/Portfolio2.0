9c822d903b14e435a5bd08b67d56bdab84e0a9da
abbeyperini <abbeyperini@gmail.com>
2021-08-25 19:04:34 -0400

    Error graphic animation

--- portfolio/src/components/Error.js
+++ portfolio/src/components/Error.js
@@ -4,7 +4,7 @@ import {ReactComponent as ConfusedShiba} from '../images/confused-shiba-inu.svg'
 function Error() {
   return (
     <div className="container_error">
-      <ConfusedShiba className="error-graphic" />
+      <ConfusedShiba className="error-graphic"/>
       <p>There was an error! Try again later.</p>
     </div>
   )
