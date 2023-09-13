452a7e48d730956decbc57694415ba7fb5b47c11
abbeyperini <abbeyperini@gmail.com>
2023-09-13 19:31:37 -0400

    add sticker shop link

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -12,6 +12,7 @@ function NavBar(props) {
         <Link to="/portfolio" className="nav_button" >Portfolio</Link>
         <Link to="/about" className="nav_button">About</Link>
         <Link to="/contact" className="nav_button">Contact</Link>
+        <Link to="https://abbey-perinis-stickers.printify.me/products" className="nav_button">Stickers</Link>
         <Toggle setClassName={props.setClassName} />
       </nav>
     )
