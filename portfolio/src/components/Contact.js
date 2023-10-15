678431a5b1dc80d1ac682b1d729e47bd6ecad606
abbeyperini <abbeyperini@gmail.com>
2023-10-15 11:20:10 -0400

    add insta link

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -3,6 +3,7 @@ import SingleWrapper from "./SingleWrapper";
 import { ReactComponent as ExternalLink } from '../images/external-link.svg';
 import TwitterLogo from '../images/Twitter.png';
 import LinkedInLogo from '../images/Linked-logo.png';
+import InstagramLogo from '../images/insta-icon.png';
 import { ReactComponent as DEVLogo } from '../images/devto.svg';
 import HashnodeLogo from '../images/hashnode-icon.png';
 
@@ -44,6 +45,7 @@ function Contact({home}) {
                 <ul className="contact_list">
                   <li><a className="link-new-tab" aria-label="link to open Abbey's Twitter profile in a new tab" href="https://twitter.com/AbbeyPerini" target="_blank" rel="noreferrer">Twitter<ExternalLink className="external-link" id="TwitterExternalLink" focusable="false"/></a><img src={TwitterLogo} alt="" className="contact-logo"/></li>
                   <li><a className="link-new-tab" aria-label="link to open Abbey's LinkedIn profile in a new tab" href="https://www.linkedin.com/in/abbey-perini/" target="_blank" rel="noreferrer">LinkedIn<ExternalLink className="external-link" id="LinkedInExternalLink" focusable="false"/></a><img src={LinkedInLogo} alt="" className="contact-logo linked-in"/></li>
+                  <li><a className="link-new-tab" aria-label="link to open Abbey's Instagram profile in a new tab" href="https://www.instagram.com/abbeyperinicodes/" target="_blank" rel="noreferrer">Instagram<ExternalLink className="external-link" id="InstagramExternalLink" focusable="false"/></a><img src={InstagramLogo} alt="" className="contact-logo instagram"/></li>
                   <li><a href="https://dev.to/abbeyperini" target="_blank" rel="noreferrer" aria-label="link to Abbey's Profile on DEV">DEV</a><ExternalLink className="external-link" id="DEVExternalLink" focusable="false"/><DEVLogo className="contact-logo" focusable="false" /></li>
                   <li><a href="https://abbeyperini.hashnode.dev/" target="_blank" rel="noreferrer" aria-label="link to Abbey's Profile on Hashnode">Hashnode</a><ExternalLink className="external-link" id="HashnodeExternalLink" focusable="false"/><img src={HashnodeLogo} alt="" className="contact-logo"/></li>
                 </ul>
