75c22c433743e87059d040e6d265da2acb78dd10
abbeyperini <abbeyperini@gmail.com>
2024-07-22 15:24:20 -0400

    Add connect.tech to speaking, remove external links and icons, update shop blurb

--- portfolio/src/components/Content.js
+++ portfolio/src/components/Content.js
@@ -12,7 +12,7 @@ function Content({home}) {
               <img src={devShiba} className="content-graphic" alt="a shiba inu wearing glasses and a bowtie and working on a laptop covered in stickers" />
               <p>You can find my writing on <a href="https://dev.to/abbeyperini" rel="noreferrer">DEV</a> and <a href="https://abbeyperini.hashnode.dev/" rel="noreferrer">Hashnode</a>, and in my <a href="https://www.abbeyperini.com" rel="noreferrer">digital garden</a> (a work in progress).</p>
               <p>In 2024, my goal is to get better at making videos, which you can find on <a href="https://www.tiktok.com/@abbeyperinicodes/" rel="noreferrer">TikTok</a> and <a href="https://www.youtube.com/channel/UCJYky0mE6tqM9hMDsQAIrEg" rel="noreferrer">YouTube</a>.</p>
-              <p>If you want stickers, like the developer shiba used here, check out my <a href="https://abbeyperini.shop" rel="noreferrer" >shop</a>.</p>
+              <p>If you have found my content valuable and want to show your appreciation, you can <a href="https://abbeyperini.shop" rel="noreferrer" >send me a lil treat</a>.</p>
             </div>
         </section>
       </SingleWrapper>
--- portfolio/src/components/Speaking.js
+++ portfolio/src/components/Speaking.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import SingleWrapper from "./SingleWrapper";
-import { ReactComponent as ExternalLink } from '../images/external-link.svg';
 import { ReactComponent as Arrow } from '../images/arrow.svg';
 
 function Speaking({home}) {
@@ -12,20 +11,21 @@ function Speaking({home}) {
             <div className="talks secondary-container">
               <h3>Upcoming Talks</h3>
               <ul className="talk-list">
-                <li><a href="https://thatconference.com/activities/Kol7lX7IoKykdx4xv6jR" target="_blank" rel="noreferrer" className="talk_link"><p className="event_title">THAT WI</p><ExternalLink className="external-link" id="THATLink" focusable="false"/></a><p>July 31, 2024</p></li>
-                <li><a href="https://www.meetup.com/modernwebatl/events/302355679" className="talk_link"><p className="event_title">Modern Web + ATLJS event</p><ExternalLink className="external-link" id="ModernWebLink" focusable="false"/></a><p>August 13, 2024</p></li>
-                <li><a href="https://www.netlify.com/compose/" target="_blank" rel="noreferrer" className="talk_link"><p className="event_title">Netlify Compose</p><ExternalLink className="external-link" id="ComposeLink" focusable="false"/></a><p>October 3, 2024</p></li>
-                <li><a href="https://javascript-conference.com/general-web-development/beginners-guide-http/" target="_blank" rel="noreferrer" className="talk_link"><p className="event_title">iJS</p><ExternalLink className="external-link" id="iJsLink" focusable="false"/></a><p>October 8, 2024</p></li>
-                <li><a href="https://2024.allthingsopen.org/speakers/abbey-perini" target="_blank" rel="noreferrer" className="talk_link"><p className="event_title">All Things Open</p><ExternalLink className="external-link" id="ATOLink" focusable="false"/></a><p>October 27-29, 2024</p></li>
+                <li><a href="https://thatconference.com/activities/Kol7lX7IoKykdx4xv6jR" rel="noreferrer" className="talk_link"><p className="event_title">THAT WI</p></a><p>July 31, 2024</p></li>
+                <li><a href="https://www.meetup.com/modernwebatl/events/302355679" className="talk_link"><p className="event_title">Modern Web + ATLJS event</p></a><p>August 13, 2024</p></li>
+                <li><a href="https://www.netlify.com/compose/" rel="noreferrer" className="talk_link"><p className="event_title">Netlify Compose</p></a><p>October 3, 2024</p></li>
+                <li><a href="https://javascript-conference.com/general-web-development/beginners-guide-http/" rel="noreferrer" className="talk_link"><p className="event_title">iJS</p></a><p>October 8, 2024</p></li>
+                <li><a href="https://2024.allthingsopen.org/speakers/abbey-perini" rel="noreferrer" className="talk_link"><p className="event_title">All Things Open</p></a><p>October 27-29, 2024</p></li>
+                <li><a href="https://2024.connect.tech/session/682054" rel="noreferrer" className="talk_link"><p className="event_title">Connect.Tech</p></a><p>November 18-20, 2024</p></li>
               </ul>
               <h3>Recordings of Past Talks</h3>
               <ul className="talk-list">
-                <li><p className="event_title">TechLead Conference 2024</p><a href="https://portal.gitnation.org/contents/cognitive-load-and-your-development-environment" target="_blank" rel="noreferrer" className="talk_link"><p>Cognitive Load and Your Developent Environment</p><ExternalLink className="external-link" id="TechLeadLink" focusable="false"/></a></li>
-                <li><p className="event_title">StirTrek 2024</p><a href="https://www.youtube.com/watch?v=jghWtOIYP54&t=6s&ab_channel=StirTrek" target="_blank" rel="noreferrer" className="talk_link"><p>#gitPanic - Merging and Rebasing</p><ExternalLink className="external-link" id="StirTrekLink" focusable="false"/></a></li>
-                <li><p className="event_title">MagnoliaJS 2023</p><a href="https://youtu.be/mr6rWFgh_1E?t=20463" target="_blank" rel="noreferrer" className="talk_link"><p>Cognitive Load and Your Development Environment</p><ExternalLink className="external-link" id="MagnoliaLink" focusable="false"/></a></li>
-                <li><p className="event_title">The Monthly dev #32</p><a href="https://www.youtube.com/watch?v=XWW1hQEnERQ&ab_channel=dailydev" target="_blank" rel="noreferrer" className="talk_link"><p>Getting Started in a New Codebase</p><ExternalLink className="external-link" id="MonthlyDevLink" focusable="false"/></a></li>
-                <li><p className="event_title">JSDrops</p><a href="https://dropjs.com/from-idea-to-design-for-non-designers-with-abbey-perini" target="_blank" rel="noreferrer" className="talk_link"><p>From Idea to Design for Non-Designers</p><ExternalLink className="external-link" id="JSDropsLink" focusable="false"/></a></li>
-                <li><p className="event_title">Front-end Foxes Day 2022</p><a href="https://cfe.dev/sessions/fefd2022-semantic-html/" target="_blank" rel="noreferrer" className="talk_link"><p>Semantic HTML: What, Why, and How</p><ExternalLink className="external-link" id="HTMLLink" focusable="false"/></a></li>
+                <li><p className="event_title">TechLead Conference 2024</p><a href="https://portal.gitnation.org/contents/cognitive-load-and-your-development-environment" rel="noreferrer" className="talk_link"><p>Cognitive Load and Your Developent Environment</p></a></li>
+                <li><p className="event_title">StirTrek 2024</p><a href="https://www.youtube.com/watch?v=jghWtOIYP54&t=6s&ab_channel=StirTrek" rel="noreferrer" className="talk_link"><p>#gitPanic - Merging and Rebasing</p></a></li>
+                <li><p className="event_title">MagnoliaJS 2023</p><a href="https://youtu.be/mr6rWFgh_1E?t=20463" rel="noreferrer" className="talk_link"><p>Cognitive Load and Your Development Environment</p></a></li>
+                <li><p className="event_title">The Monthly dev #32</p><a href="https://www.youtube.com/watch?v=XWW1hQEnERQ&ab_channel=dailydev" rel="noreferrer" className="talk_link"><p>Getting Started in a New Codebase</p></a></li>
+                <li><p className="event_title">JSDrops</p><a href="https://dropjs.com/from-idea-to-design-for-non-designers-with-abbey-perini" rel="noreferrer" className="talk_link"><p>From Idea to Design for Non-Designers</p></a></li>
+                <li><p className="event_title">Front-end Foxes Day 2022</p><a href="https://cfe.dev/sessions/fefd2022-semantic-html/" rel="noreferrer" className="talk_link"><p>Semantic HTML: What, Why, and How</p></a></li>
               </ul>
               <a href="/contact" className="speaking_CTA">Invite Me to Speak <Arrow className="button_arrow" focusable="false"/></a>
             </div>
