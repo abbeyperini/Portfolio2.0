85f0911e739e3a5282d2def76812bb24cc612b23
abbeyperini <abbeyperini@gmail.com>
2024-07-24 17:22:18 -0400

    More descriptive color mode toggle aria label

--- portfolio/src/components/Toggle.js
+++ portfolio/src/components/Toggle.js
@@ -3,10 +3,13 @@ import '../styles/toggle.css';
 import { setTheme } from '../utils/themes';
 
 function Toggle({setClassName}) {
+    const darkLabel = "color mode toggle, dark mode";
+    const lightLabel = "color mode toggle, light mode";
     // false = dark mode because of the way I wrote the CSS
     const [active, setActive] = useState(false)
     // the opposite, for screenreaders
     const [ariaActive, setAriaActive] = useState(true)
+    const [ariaLabel, setAriaLabel] = useState(darkLabel)
     const theme = localStorage.getItem('theme')
 
     const changeThemeAndToggle = () => {
@@ -14,10 +17,12 @@ function Toggle({setClassName}) {
         setTheme('theme-light', setClassName)
         setActive(true)
         setAriaActive(false)
+        setAriaLabel(lightLabel)
       } else {
         setTheme('theme-dark', setClassName)
         setActive(false)
         setAriaActive(true)
+        setAriaLabel(darkLabel);
       }
     }
 
@@ -35,16 +40,18 @@ function Toggle({setClassName}) {
       if (theme === 'theme-dark') {
         setActive(false)
         setAriaActive(true)
+        setAriaLabel(darkLabel);
       } else if (theme === 'theme-light') {
         setActive(true)
         setAriaActive(false)
+        setAriaLabel(lightLabel);
       }
     }, [theme])
 
     return (
-      <div className="container--toggle" title="dark mode toggle">
+      <div className="container--toggle" title="color mode toggle">
         <input role="switch" aria-checked={ariaActive} onKeyDown={handleKeypress} type="checkbox" id="toggle" className="toggle--checkbox" onClick={handleOnClick} checked={active} readOnly />
-        <label htmlFor="toggle" className="toggle--label" aria-label="dark mode toggle">
+        <label htmlFor="toggle" className="toggle--label" aria-label={ariaLabel}>
           <span className="toggle--label-background"></span>
         </label>
       </div>
