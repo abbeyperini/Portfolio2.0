1ebcb37ceff183df6e8e5db7cf2ee9b10f404b9c
abbeyperini <abbeyperini@gmail.com>
2025-01-23 17:14:15 -0500

    Update contact

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -2,11 +2,7 @@ import React, { useState } from 'react';
 import SingleWrapper from "./SingleWrapper";
 import { ReactComponent as ExternalLink } from '../images/external-link.svg';
 import BlueskyLogo from '../images/Bluesky.png';
-import TwitterLogo from '../images/Twitter.png';
 import LinkedInLogo from '../images/Linked-logo.png';
-import InstagramLogo from '../images/insta-icon.png';
-import ThreadsLogo from '../images/threads-logo.png';
-import TikTokLogo from '../images/tiktok-logo.png';
 import YouTubeLogo from '../images/YouTube.png';
 import { ReactComponent as DEVLogo } from '../images/devto.svg';
 import HashnodeLogo from '../images/hashnode-icon.png';
@@ -48,15 +44,13 @@ function Contact({home}) {
                 <h3 className="contact_text">Connect with me here:</h3>
                 <ul className="contact_list">
                   <li><a className="link-new-tab" aria-label="link to open Abbey's Bluesky profile in a new tab" href="https://bsky.app/profile/abbeyperini.dev" target="_blank" rel="noreferrer">Bluesky<ExternalLink className="external-link" id="BlueskyExternalLink" focusable="false"/></a><img src={BlueskyLogo} alt="" className="contact-logo"/></li>
-                  <li><a className="link-new-tab" aria-label="link to open Abbey's Twitter profile in a new tab" href="https://twitter.com/AbbeyPerini" target="_blank" rel="noreferrer">Twitter<ExternalLink className="external-link" id="TwitterExternalLink" focusable="false"/></a><img src={TwitterLogo} alt="" className="contact-logo"/></li>
                   <li><a className="link-new-tab" aria-label="link to open Abbey's LinkedIn profile in a new tab" href="https://www.linkedin.com/in/abbey-perini/" target="_blank" rel="noreferrer">LinkedIn<ExternalLink className="external-link" id="LinkedInExternalLink" focusable="false"/></a><img src={LinkedInLogo} alt="" className="contact-logo linked-in"/></li>
-                  <li><a className="link-new-tab" aria-label="link to open Abbey's Instagram profile in a new tab" href="https://www.instagram.com/abbeyperinicodes/" target="_blank" rel="noreferrer">Instagram<ExternalLink className="external-link" id="InstagramExternalLink" focusable="false"/></a><img src={InstagramLogo} alt="" className="contact-logo instagram"/></li>
-                  <li><a className="link-new-tab" aria-label="link to open Abbey's Threads profile in a new tab" href="https://www.threads.net/@abbeyperinicodes/" target="_blank" rel="noreferrer">Threads<ExternalLink className="external-link" id="ThreadsExternalLink" focusable="false"/></a><img src={ThreadsLogo} alt="" className="contact-logo threads"/></li>
-                  <li><a className="link-new-tab" aria-label="link to open Abbey's TikTok profile in a new tab" href="https://www.tiktok.com/@abbeyperinicodes/" target="_blank" rel="noreferrer">TikTok<ExternalLink className="external-link" id="TikTokExternalLink" focusable="false"/></a><img src={TikTokLogo} alt="" className="contact-logo tiktok"/></li>
                   <li><a className="link-new-tab" aria-label="link to open Abbey's YouTube profile in a new tab" href="https://www.youtube.com/channel/UCJYky0mE6tqM9hMDsQAIrEg" target="_blank" rel="noreferrer">YouTube<ExternalLink className="external-link" id="YouTubeExternalLink" focusable="false"/></a><img src={YouTubeLogo} alt="" className="contact-logo youtube"/></li>
                   <li><a href="https://dev.to/abbeyperini" target="_blank" rel="noreferrer" aria-label="link to Abbey's Profile on DEV">DEV</a><ExternalLink className="external-link" id="DEVExternalLink" focusable="false"/><DEVLogo className="contact-logo" focusable="false" /></li>
                   <li><a href="https://abbeyperini.hashnode.dev/" target="_blank" rel="noreferrer" aria-label="link to Abbey's Profile on Hashnode">Hashnode</a><ExternalLink className="external-link" id="HashnodeExternalLink" focusable="false"/><img src={HashnodeLogo} alt="" className="contact-logo"/></li>
                 </ul>
+                <p>I do not have accounts on ByteDance or Meta products.</p>
+                <p className="contact_p">I'm working on hosting my tweets on <a href="https://abbeyperini.com">my digital garden</a>.</p>
                 <h3>Fill out the form to send me an email!</h3>
                 <form className="contact-form" onSubmit={handleSubmit}>
                     <p>
