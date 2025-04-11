3aebdc36f600ff86b21a883824d9ec54f7c31aaf
abbeyperini <abbeyperini@gmail.com>
2025-04-11 18:03:30 -0400

    focusable="true" isn't needed inside a link

--- portfolio/src/components/work/DigitalGarden.js
+++ portfolio/src/components/work/DigitalGarden.js
@@ -11,8 +11,8 @@ function DigitalGarden() {
             <img id="dGScreenshot" className="work_image" src={dGScreenshot} alt="screenshot of digital garden site"/>
             <p className="work_text">If you want a comprehensive summary of digital gardening, you can't go wrong with Maggie Appleton's <a href="https://maggieappleton.com/garden-history" target="_blank" rel="noreferrer">A Brief History & Ethos of the Digital Garden</a> or her <a href="https://github.com/MaggieAppleton/digital-gardeners" target="_blank" rel="noreferrer">repo of resources</a>. As it stands, my blog timeline would give you topic whiplash. In contrast, a digital garden works with how my brain works - grouping related articles together even if I wrote them 5 years apart. Still a work in progress, My digital garden is to be an organic home for all my tech creations. My stack is JavaScript, CSS, HTML, and Markdown.</p>
             <span className="work_links">
-                <a className="link-new-tab" href="https://github.com/abbeyperini/digital-garden" target="_blank" rel="noreferrer"><Github id="DigitalGardenGithub" aria-label="open Digital Garden Github repository in a new tab" focusable="true" className="work_icon"/></a>
-                <a className="link-new-tab" href="https://abbeyperini.com" target="_blank" rel="noreferrer"><Chrome id="DigitalGardenChrome" aria-label="open Digital Garden website in a new tab" focusable="true" className="work_icon"/></a>
+                <a className="link-new-tab" href="https://github.com/abbeyperini/digital-garden" target="_blank" rel="noreferrer"><Github id="DigitalGardenGithub" aria-label="open Digital Garden Github repository in a new tab" className="work_icon"/></a>
+                <a className="link-new-tab" href="https://abbeyperini.com" target="_blank" rel="noreferrer"><Chrome id="DigitalGardenChrome" aria-label="open Digital Garden website in a new tab" className="work_icon"/></a>
             </span>
         </div>
     )
--- portfolio/src/components/work/ThisSite.js
+++ portfolio/src/components/work/ThisSite.js
@@ -21,7 +21,7 @@ function ThisSite() {
                
             </p>
             <span className="work_links">
-                <a className="link-new-tab" href="https://github.com/abbeyperini/Portfolio2.0" target="_blank" rel="noreferrer"><Github id="ThisSiteGithub" aria-label="open Github repository for this site in a new tab" focusable="true" className="work_icon"/></a>
+                <a className="link-new-tab" href="https://github.com/abbeyperini/Portfolio2.0" target="_blank" rel="noreferrer"><Github id="ThisSiteGithub" aria-label="open Github repository for this site in a new tab" className="work_icon"/></a>
             </span>
         </div>
     )
