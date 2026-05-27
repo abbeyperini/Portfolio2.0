548830ba946d6d45a76354c0cca718b9b2dac4ad
AbbeyPerini <abbeyperini@gmail.com>
2026-05-27 15:39:09 -0400

    Update speaking

--- portfolio/src/components/Speaking.js
+++ portfolio/src/components/Speaking.js
@@ -11,7 +11,10 @@ function Speaking({home}) {
             <div className="talks secondary-container">
               <h3>Upcoming Talks</h3>
               <ul className="talk-list">
-                <li><p className="event_title">VueConfUS 2026</p><a href="https://vueconf.us/#speakers" className="talk_link">Universal Lessons Learned from Legacy Codebases</a></li>
+                <li><p className="event_title">React Summit 2026</p><a href="https://reactsummit.com/#speakers" className="talk_link">What the First Rule of ARIA Really Means</a></li>
+                <li><p className="event_title">Nebraska.code() 2026</p><a href="https://www.nebraska-code.com/#speakers" className="talk_link">Restoring Lost Work in Git: A Forbidden Love Story and Slots, Slots, Slots, Everybody!</a></li>
+                <li><p className="event_title">KCDC 2026</p><a href="https://www.kcdc.info/speakers" className="talk_link">State Management in Front-end Development Workshop</a></li>
+                <li><p className="event_title">Techbash 2026</p><a href="https://techbash.com/sessions" className="talk_link">From Idea to Design for Non-Designers and Restoring Lost Work in Git: A Forbidden Love Story</a></li>
               </ul>
               <h3>Testimonials</h3>
               <ul>
