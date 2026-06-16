6f0e806e6d9ecf0f4df58d480c54d66f9472b5e6
AbbeyPerini <abbeyperini@gmail.com>
2026-06-16 09:36:56 -0400

    Update speaking

--- portfolio/src/components/Speaking.js
+++ portfolio/src/components/Speaking.js
@@ -11,10 +11,9 @@ function Speaking({home}) {
             <div className="talks secondary-container">
               <h3>Upcoming Talks</h3>
               <ul className="talk-list">
-                <li><p className="event_title">React Summit 2026</p><a href="https://reactsummit.com/#speakers" className="talk_link">What the First Rule of ARIA Really Means</a></li>
                 <li><p className="event_title">Nebraska.code() 2026</p><a href="https://www.nebraska-code.com/#speakers" className="talk_link">Restoring Lost Work in Git: A Forbidden Love Story and Slots, Slots, Slots, Everybody!</a></li>
                 <li><p className="event_title">KCDC 2026</p><a href="https://www.kcdc.info/speakers" className="talk_link">State Management in Front-end Development Workshop</a></li>
-                <li><p className="event_title">TechBash 2026</p><a href="https://techbash.com/sessions" className="talk_link">From Idea to Design for Non-Designers and Restoring Lost Work in Git: A Forbidden Love Story</a></li>
+                <li><p className="event_title">TechBash 2026</p><a href="https://techbash.com/sessions" className="talk_link">Restoring Lost Work in Git: A Forbidden Love Story and From Idea to Design for Non-Designers</a></li>
               </ul>
               <h3>Testimonials</h3>
               <ul>
