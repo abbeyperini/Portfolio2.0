1b99704afce36002e1d60d77c4037256ad146402
AbbeyPerini <abbeyperini@gmail.com>
2026-06-22 16:23:09 -0400

    Update speaking

--- portfolio/src/components/Speaking.js
+++ portfolio/src/components/Speaking.js
@@ -25,8 +25,8 @@ function Speaking({home}) {
               </ul>
               <h3>Recordings of Past Talks</h3>
               <ul className="talk-list">
-                <li><p className="event_title">React Summit 2025</p><a href="https://gitnation.com/contents/slots-slots-slots-everybody" rel="noreferrer" className="talk_link">Slots, Slots, Slots, Everybody!</a></li>
-                <li><p className="event_title">Frontend Queens</p><a href="https://www.youtube.com/watch?v=cZJ19XzS3ng" rel="noreferrer" className="talk_link">Global Accessibility Awareness Day: Building an Inclusive Web</a></li>
+                <li><p className="event_title">React Summit Amsterdam 2026</p><a href="https://gitnation.com/contents/what-the-first-rule-of-aria-really-means" rel="noreferrer" className="talk_link">What the First Rule of ARIA Really Means</a></li>
+                <li><p className="event_title">React Summit US 2025</p><a href="https://gitnation.com/contents/slots-slots-slots-everybody" rel="noreferrer" className="talk_link">Slots, Slots, Slots, Everybody!</a></li>
                 <li><p className="event_title">dotJS 2025</p><a href="https://www.youtube.com/watch?si=RLUgFmQFeyEjVyrg&v=mt2v2DX5Tno&feature=youtu.be" rel="noreferrer" className="talk_link">Coding and ADHD: Where We Excel</a></li>
                 <li><p className="event_title">TechLead Conference 2024</p><a href="https://portal.gitnation.org/contents/cognitive-load-and-your-development-environment" rel="noreferrer" className="talk_link"><p>Cognitive Load and Your Development Environment</p></a></li>
                 <li><p className="event_title">StirTrek 2024</p><a href="https://www.youtube.com/watch?v=jghWtOIYP54&t=6s&ab_channel=StirTrek" rel="noreferrer" className="talk_link"><p>#gitPanic - Merging and Rebasing</p></a></li>
@@ -35,7 +35,8 @@ function Speaking({home}) {
               </ul>
               <h3>Interviews and More</h3>
               <ul className="talk-list">
-                <li><a href="https://overcommitted.dev/accessibility-fiber-arts-and-adhd-with-abbey-perini/">Overcommitted Podcast</a></li>
+                <li><a href="https://overcommitted.dev/accessibility-fiber-arts-and-adhd-with-abbey-perini/" rel="noreferrer" className="talk_link">Overcommitted Podcast</a></li>
+                <li><a href="https://www.youtube.com/watch?v=cZJ19XzS3ng" rel="noreferrer" className="talk_link">Frontend Queens' Global Accessibility Awareness Day: Building an Inclusive Web</a></li>
                 <li><a href="https://www.youtube.com/watch?si=QrlWH64qghXtDpe1&v=rgt5GPFgryI&feature=youtu.be" rel="noreferrer" className="talk_link">FreeCodeCamp Podcast</a></li>
                 <li><a href="https://codetv.dev/series/web-dev-challenge/s2/e1-build-a-custom-api-postman" rel="noreferrer" className="talk_link">Web Dev Challenge</a></li>
                 <li><a href="https://youtu.be/C_E4xghf-8Y?t=20525" rel="noreferrer" className="talk_link">Developers of the World with James Q. Quick</a></li>
